05ec3946c85752e23d5d3f943981161341542263
GitHub Action <action@github.com>
2021-09-21 22:18:42 +0000

    Template cleanup

--- src/main/kotlin/com/github/montecreasor/myplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/montecreasor/myplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.montecreasor.myplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.montecreasor.myplugin.MyBundle
 
 class MyApplicationService {
 
